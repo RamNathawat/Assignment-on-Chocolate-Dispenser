a324a8e740d3025849883172e53629ee70e27b3e
Ram nathawat <nathawatr7@gmail.com>
2023-11-30 11:58:57 +0530

    Done till progression 4

--- src/app.js
+++ src/app.js
@@ -103,15 +103,59 @@ var chocolates = [
 
 // x and y ==> can take any of the values from the below list-
 // [ green, red, purple, blue, crimson, silver, pink ]
-// z ==> can take a number from 0<=a<=100
+// z ==> can take a count from 0<=a<=100
 
 //Progression 1: Add z chocolates of x color
 
+function addChocolates(chocolates, color, count) {
+if (count <= 0) {
+  return 'Number cannot be zero/negative';
+  }
+for (let i = 0; i < count; i++) {
+  chocolates.unshift(color);
+  }
+}
+
 //Progression 2: Remove z chocolates from the top the dispenser
+
+function removeChocolates(chocolates, count) {
+if (count <= 0) {
+  return 'Number cannot be zero/negative';
+}
+if (count > chocolates.length) {
+  return 'Insufficient chocolates in the dispenser';
+}
+const removedChocolates = chocolates.splice(0, count);
+return removedChocolates;
+}
 
 //Progression 3: Dispense z chocolates
 
+function dispenseChocolates(chocolates, count) {
+if (count <= 0) {
+  return 'Number cannot be zero/negative';
+}
+if (count > chocolates.length) {
+  return 'Insufficient chocolates in the dispenser';
+}
+let dischoco = chocolates.slice(-count); 
+return dischoco.reverse(); 
+}
+
 //Progression 4: Dispense z chocolates of x color
+
+function dispenseChocolatesOfColor(chocolates, count, color) {
+if (count <= 0) {
+  return 'Number cannot be zero/negative';
+}
+const colorChocolates = chocolates.filter(choco => choco === color);
+if (count > colorChocolates.length) {
+  return 'Insufficient chocolates in the dispenser';
+}
+const dispensedChocolates = colorChocolates.slice(-count);
+chocolates = chocolates.filter(choco => choco !== color || dispensedChocolates.indexOf(choco) === -1);
+return dispensedChocolates;
+}
 
 //Progression 5: Display z chocolates of each color. Return array of numbers [green, silver, blue, crimson, purple, red, pink]
 
